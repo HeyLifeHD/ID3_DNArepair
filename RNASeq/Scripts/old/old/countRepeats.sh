72d72dc5068fac8edec6a36f991d9e4488cdb128
HeyLifeHD <joschkahey@gmail.com>
2021-10-04 15:25:51 +0200

    Selected scripts that were used for manuscript.

--- RNASeq/Scripts/old/old/countRepeats.sh
+++
@@ -1,5 +0,0 @@
-##Count overlap or reads with repeats
-files=`ls c010-datasets/Internal/2018-Ali/RNASeq/181114_analysis_rep1/markDuplicates/*.bam`
-featureCounts -p -f -F 'SAF' -a /bigdisk/Nanopore/raw_data/repeats/repeats.gtf \
--o c010-datasets/Internal/2018-Ali/RNASeq/181114_analysis_rep1/repeat_counts.txt $files 
-
--- RNASeq/Scripts/old/unmerged/repeats/countRepeats.sh
+++
@@ -1,5 +0,0 @@
-##Count overlap or reads with repeats
-files=`ls icgc/analysis/C010/id3/190228_RNAseq_processing/STAR/*.bam`
-featureCounts -p -f -F 'SAF' -a /bigdisk/Nanopore/raw_data/repeats/repeats.gtf \
--o c010-datasets/Internal/2018-Ali/RNASeq/190305_unmerged/results/repeats/repeat_counts.txt $files 
-
